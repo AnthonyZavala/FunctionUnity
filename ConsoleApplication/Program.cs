db64c36d7fb920d824b390ce987b53877e80ac9f
Anthony Zavala <zavalaan1@gmail.com>
2016-10-14 06:37:54 -0700

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionUnity;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Class1();
+            x.Run().Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FunctionUnity/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace FunctionUnity
+{
+    public class Class1
+    {
+        private readonly UnityContainer _container;
+
+        public Class1()
+        {
+            _container = new UnityContainer();
+            Map<Messenger, MessengerA>();
+        }
+
+        public void Map<TFrom, TTo>()
+        {
+            Type fromType = typeof(TFrom);
+            IEnumerable<Type> delegateTypes = fromType.GetNestedTypes(BindingFlags.Public).Where(t => t.BaseType == typeof(MulticastDelegate));
+
+            Type toType = typeof(TTo);
+            var toStaticMethods = toType.GetMethods(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (Type del in delegateTypes)
+            {
+                var match = toStaticMethods.SingleOrDefault(x => x.Name == del.Name);
+                if (match != null)
+                {
+                    _container.RegisterType(del, new InjectionFactory(c => Delegate.CreateDelegate(del, match)));
+                }
+            }
+        }
+
+        public async Task Run()
+        {
+            var get = _container.Resolve<Messenger.Get>();
+            await get("Hello Del");
+        }
+    }
+
+    public abstract class Messenger
+    {
+        public delegate Task Get(string message);
+        public delegate Task Save(string message);
+    }
+
+    public class MessengerA
+    {
+        public async static Task Get(string message)
+        {
+            await Task.Run(() => Console.Write("A - " + message));
+        }
+    }
+
+    public class MessengerB
+    {
+        public async static Task Get(string message)
+        {
+            await Task.Run(() => Console.Write("B - " + message));
+        }
+    }
+}
